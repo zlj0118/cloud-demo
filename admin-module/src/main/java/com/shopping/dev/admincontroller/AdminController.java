3f823d10850f9096a2d4aab780d7e512a552a98c
孙宏宇 <923292631@qq.com>
2018-11-09 15:05:06 +0800

    拉取登录模块前提交,修正了页面中规格参数部分功能

--- admin-module/src/main/java/com/shopping/dev/admincontroller/AdminController.java
+++ admin-module/src/main/java/com/shopping/dev/admincontroller/AdminController.java
@@ -2,21 +2,31 @@ package com.shopping.dev.admincontroller;
 
 import com.shopping.dev.adminservice.AdminService;
 import com.shopping.dev.entity.ContentCategory;
-import com.shopping.dev.repository.AdminRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.shopping.dev.entity.Item;
+import com.shopping.dev.repository.BaseRepository;
+import com.shopping.dev.repository.BaseRepositoryImpl;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
-@RequestMapping("/content")
 public class AdminController {
 
     @Resource
     private AdminService adminService;
-    @RequestMapping("/category/list")
-    public List findAll() {
-        return adminService.findAll();
+
+    // 查询商品,查询tb_item
+    @GetMapping("/item/list")
+    public List<Item> findAllItem(Integer page, Integer rows) {
+        return adminService.findAllItem(page, rows);
+    }
+
+    // 内容分类管理,查询tb_content_category,返回表中全部内容集合
+    @GetMapping("/content/category/list")
+    public List<ContentCategory> findAllContentCategory() {
+        return adminService.findAllContentCategory();
     }
 }
--- admin-module/src/main/java/com/shopping/dev/adminservice/AdminServiceImpl.java
+++ admin-module/src/main/java/com/shopping/dev/adminservice/AdminServiceImpl.java
@@ -1,7 +1,13 @@
 package com.shopping.dev.adminservice;
 
 import com.shopping.dev.entity.ContentCategory;
-import com.shopping.dev.repository.AdminRepository;
+import com.shopping.dev.entity.Item;
+import com.shopping.dev.repository.AdminContentCategoryRepository;
+import com.shopping.dev.repository.AdminItemRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -10,10 +16,20 @@ import java.util.List;
 @Service
 public class AdminServiceImpl implements AdminService {
     @Resource
-    private AdminRepository adminRepository;
+    private AdminContentCategoryRepository adminContentCategoryRepository;
+    @Resource
+    private AdminItemRepository adminItemRepository;
 
     @Override
-    public List<ContentCategory> findAll() {
-        return adminRepository.findAll();
+    public List<Item> findAllItem(Integer page, Integer row) {
+        Pageable pageable =  PageRequest.of(page, row);
+//        Page<Item> all = adminItemRepository.findAll(pageable);
+//        List<Item> content = all.getContent();
+        return adminItemRepository.findAll(pageable).getContent();
+    }
+
+    @Override
+    public List<ContentCategory> findAllContentCategory() {
+        return adminContentCategoryRepository.findAll();
     }
 }
